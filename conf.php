188594a935e22ddff9ecadb13ffc185b2350c3df
Totodore <prevottheodore@gmail.com>
2022-03-26 23:41:55 +0100

    revert db connection

--- conf.php
+++
@@ -1,5 +0,0 @@
-<?php 
-$host = getenv('MYSQL_HOST');
-$user = getenv('MYSQL_USER');
-$pass = getenv('MYSQL_PASSWORD');
-$db = getenv('MYSQL_DATABASE');
--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 session_start();
-include_once "conf.php";
-$pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
-print_r($pdo->query("SELECT * FROM Bienvenue")->fetchAll());
 ?>
 <!DOCTYPE html>
 <html lang="fr">
