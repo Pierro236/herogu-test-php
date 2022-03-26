16df827a4054dc7ffb66804e43b7ceb9488ff90e
Totodore <prevottheodore@gmail.com>
2022-03-26 23:25:47 +0100

    test 2

--- conf.php
+++ conf.php
@@ -1,7 +1,5 @@
 <?php 
-$host = $_ENV["MYSQL_HOST"];
-$user = $_ENV["MYSQL_USER"];
-$pass = $_ENV["MYSQL_PASSWORD"];
-$db = $_ENV["MYSQL_DATABASE"];
-print_r($_ENV);
-phpinfo();
+$host = getenv('MYSQL_HOST');
+$user = getenv('MYSQL_USER');
+$pass = getenv('MYSQL_PASS');
+$db = getenv('MYSQL_DATABASE');
