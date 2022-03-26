2185f238d7b172e2a4fda0e913f7062fcb822c2d
Totodore <prevottheodore@gmail.com>
2022-03-26 23:18:30 +0100

    Connexion à la base de donnée en ligne

---
+++ conf.php
@@ -0,0 +1,5 @@
+<?php 
+$host = $_ENV["MYSQL_HOST"];
+$user = $_ENV["MYSQL_USER"];
+$pass = $_ENV["MYSQL_PASSWORD"];
+$db = $_ENV["MYSQL_DATABASE"];
--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+include_once "conf.php";
+$pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+print_r($pdo->query("SELECT * FROM Bienvenue")->fetchAll());
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -15,7 +18,7 @@ session_start();
 	<h1>
 		<?php echo "Hello World!"; ?>
 	</h1>
-	
+
 	<a href="./page.php">Page</a>
 </body>
 
